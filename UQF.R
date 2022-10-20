132954c5bd24ec0efa93214e4f1ef81317f2efeb
thammatthew <matthewtham2002@gmail.com>
2022-10-21 01:26:50 +0800

    Update UQF.R

--- UQF.R
+++ UQF.R
@@ -73,6 +73,39 @@ read_uqf <- function(uqf_zip_path, verify = T) {
   return(qtbl)
 }
 
+uqf_to_iqf <- function(uqf_tbl, mode = "strict", custom_alpha_regex, custom_sep_regex) {
+  switch(mode,
+         "lenient" = {
+           alpha_regex = "[A-Za-z]"
+           sep_regex = "[\\.\\)\\|\\-:;. ] ?"
+         },
+         "custom" = {
+           alpha_regex = custom_alpha_regex
+           sep_regex = custom_sep_regex
+         },
+         {
+           alpha_regex = "[A-Z]"
+           sep_regex = "\\. "
+         })
+  
+  iqf_tbl <- uqf_tbl %>%
+    rowwise() %>%
+    mutate(
+      Op_Ans = list(tibble(
+        Alpha = str_extract_all(Options, 
+                                regex(paste0("^", alpha_regex, "(?=", sep_regex, ")"),
+                                multiline = T)) %>% unlist,
+        Choice = str_extract_all(Options, 
+                                 regex(paste0("(?<=^", alpha_regex, sep_regex, ")(.*?)(?=^", alpha_regex, sep_regex, "|\\Z)"),
+                                 multiline = T, 
+                                 dotall = T)) %>% unlist %>% str_trim,
+        Correct = Alpha %in% (str_split(Answers, ",") %>% unlist %>% str_trim)
+      ))
+    ) %>%
+    select(-Options, -Answers)
+  return(iqf_tbl)
+}
+
 qtbl_to_html <- function(qtbl, randomise_op = F, image_dir = "images", output_filename = "questions") {
   qtbl_html <- qtbl %>%
     mutate(
@@ -135,19 +168,77 @@ qtbl_to_html <- function(qtbl, randomise_op = F, image_dir = "images", output_fi
   return(qtbl_html)
 }
 
-img_refs_to_html <- function(string, image_dir) {
-  return(str_replace_all(string, "(\\[\\[)(.*)(\\]\\])", paste('<img src="', image_dir, '/\\2">', sep = "")))
+iqf_to_html <- function(iqf_tbl, randomise_op = F, image_dir = "images", output_filename = "questions") {
+  html_tbl <- iqf_tbl %>%
+    mutate(
+      Question = img_refs_to_html(Question, image_dir),
+      Explanation = img_refs_to_html(Explanation, image_dir),
+      Op_Ans = list(Op_Ans %>%
+                      mutate(
+                        Choice = img_refs_to_html(Choice, image_dir)
+                      ))
+    )
+  
+  if(randomise_op == T) {
+    html_tbl <- html_tbl %>%
+      mutate(
+        Op_Ans = list(Op_Ans %>%
+                        slice_sample(prop = 1) %>%
+                        mutate(
+                          Alpha = LETTERS[1:length(Alpha)]
+                        )
+        )
+      )
+  }
+  
+  html_tbl <- html_tbl %>%
+    rowid_to_column(var = "Q_No") %>%
+    mutate(
+      Q_No = paste("Q", Q_No, ") ", sep = ""),
+      Question_html = paste('<div class="question">', Q_No, Question, '</div>', sep = ""),
+      Explanation_html = paste('<div class="explanation">', Explanation, '</div>', sep = ""),
+      Op_Ans = list(Op_Ans %>%
+                      mutate(
+                        Choice_html = paste0('<div class="option">', Alpha, '. ', Choice, '</div>'),
+                        Choice_Ans_html = if_else(Correct, 
+                                                  paste0('<div class="option correct">', Alpha, '. ', Choice, '</div>'), 
+                                                  paste0(Choice_html))
+                      )
+      ),
+      Options_html = paste('<div class="option_container">', paste(Op_Ans$Choice_html, collapse = ""), '</div>', sep = "\n"),
+      Op_Ans_html = paste('<div class="option_container">', paste(Op_Ans$Choice_Ans_html, collapse = ""), '</div>', sep = "\n"),
+      Final_Q_html = paste('<div class="question_container">', Question_html, Options_html, '</div>', sep = "\n"),
+      Final_QnA_html = paste('<div class="question_container">', Question_html, Op_Ans_html, Explanation_html, '</div>', sep = "\n")
+    )
+  
+  html_head =
+    '<head>
+    <link rel="stylesheet" href="styles.css"> 
+  </head>'
+  
+  q_output_filename = paste(output_filename, "_Q", sep = "")
+  qna_output_filename = paste(output_filename, "_QnA", sep = "")
+  
+  write_lines(paste(html_head, paste(html_tbl$Final_Q_html, collapse = "\n"), sep = "\n"), paste(q_output_filename, ".html", sep = ""))
+  write_lines(paste(html_head, paste(html_tbl$Final_QnA_html, collapse = "\n"), sep = "\n"), paste(qna_output_filename, ".html", sep = ""))
+  
+  return(html_tbl)
 }
 
 img_refs_to_lowercase <- function(string, image_dir) {
   return(str_replace_all(string, "(\\[\\[)(.*)(\\]\\])", replacement = tolower))
 }
 
-uqf_to_html <- function(uqf_zip_path, verify = T, randomise_op = F) {
+img_refs_to_html <- function(string, image_dir) {
+  return(str_replace_all(string, "(\\[\\[)(.*)(\\]\\])", paste('<img src="', image_dir, '/\\2">', sep = "")))
+}
+
+uqf_to_html <- function(uqf_zip_path, verify = T, mode = "strict", randomise_op = F) {
   uqf_name <- file_path_sans_ext(basename(uqf_zip_path))
-  qtbl <- read_uqf(uqf_zip_path, verify = verify)
-  qtbl_html <- qtbl_to_html(qtbl, randomise_op = randomise_op, image_dir = uqf_name, output_filename = uqf_name)
+  uqf_tbl <- read_uqf(uqf_zip_path, verify = verify)
+  iqf_tbl <- uqf_to_iqf(uqf_tbl, mode = mode)
+  html_tbl <- iqf_to_html(iqf_tbl, randomise_op = randomise_op, image_dir = uqf_name, output_filename = uqf_name)
   return(list(
-    qtbl = qtbl, 
-    qtbl_html = qtbl_html))
+    iqf = iqf_tbl, 
+    html_tbl = html_tbl))
 }
