5eb9f4be01df22a0ff1c8d63b32eb58d97c0d2f0
thammatthew <matthewtham2002@gmail.com>
2023-04-10 17:08:13 +0800

    Update UQF.R
    Switch to pandoc markdown

--- UQF.R
+++ UQF.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(tools)
 library(readxl)
 library(commonmark)
+library(pandoc)
 
 read_uqf <- function(uqf_zip_path, verify = T) {
   uqf_dir <- file_path_sans_ext(uqf_zip_path)
@@ -202,12 +203,19 @@ img_refs_to_html <- function(string, image_dir) {
   return(str_replace_all(string, "(\\[\\[)(.*?)(\\]\\])", paste('<img src="', image_dir, '/\\2">', sep = "")))
 }
 
+# markdown_to_html <- function(string) {
+#   return(markdown_html(string,
+#                        hardbreaks = T,
+#                        smart = T,
+#                        extensions = T
+#                        ))
+# }
+
 markdown_to_html <- function(string) {
-  return(markdown_html(string,
-                       hardbreaks = T,
-                       smart = T,
-                       extensions = T
-                       ))
+  return(pandoc_convert(text = string,
+                       from = "markdown",
+                       to = "html"
+  ))
 }
 
 uqf_to_html <- function(uqf_zip_path, 
